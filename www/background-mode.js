573dd6995136a9534ba8ab0ff1b842cc72fe07aa
tiperes <66407614+tiperes@users.noreply.github.com>
2026-02-05 17:05:33 +0000

    Android 6-14+ behaviour fixes
    Overall refactoring/review to comply with Android 6-14+ behaviours/quirks when using foreground services, namelly the aggressive constrainst of the newer Android versions. 
    
    1 - The need to call startForeground asao to avoid "android.app.RemoteServiceException$ForegroundServiceDidNotStartInTimeException"
    
    2 - The limitations and edge cases that can occur when stopping/starting the foreground service multiple times in Android 14+ devices.
    To work without issues:
    2.1 - During the application's lifecycle, you should not reenable the service after it has been toogled from enabled to disabled. By default the service is considered disabled, as previsouly.
    2.2 - Foreground service Notification can not longer be silent nor will be canceled/dismissed when the app comes back to foreground.

--- www/background-mode.js
+++ www/background-mode.js
@@ -23,284 +23,36 @@ var exec    = require('cordova/exec'),
     channel = require('cordova/channel');
 
 /**
- * Activates the background mode. When activated the application
- * will be prevented from going to sleep while in background
- * for the next time.
+ * @private
+ *
+ * Initialize the plugin.
+ *
+ * Method should be called after the 'deviceready' event
+ * but before the event listeners will be called.
  *
  * @return [ Void ]
  */
-exports.enable = function()
+exports._pluginInitialize = function()
 {
-    if (this.isEnabled())
-        return;
-
-    var fn = function() {
-        exports._isEnabled = true;
-        exports.fireEvent('enable');
-    };
-
-    cordova.exec(fn, null, 'BackgroundMode', 'enable', []);
-};
-
-/**
- * Deactivates the background mode. When deactivated the application
- * will not stay awake while in background.
- *
- * @return [ Void ]
- */
-exports.disable = function()
-{
-    if (!this.isEnabled())
-        return;
-
-    var fn = function() {
-        exports._isEnabled = false;
-        exports.fireEvent('disable');
-    };
-
-    cordova.exec(fn, null, 'BackgroundMode', 'disable', []);
-};
-
-/**
- * Enable or disable the background mode.
- *
- * @param [ Bool ] enable The status to set for.
- *
- * @return [ Void ]
- */
-exports.setEnabled = function (enable)
-{
-    if (enable) {
-        this.enable();
-    } else {
-        this.disable();
-    }
-};
-
-/**
- * List of all available options with their default value.
- *
- * @return [ Object ]
- */
-exports.getDefaults = function()
-{
-    return this._defaults;
-};
-
-/**
- * The actual applied settings.
- *
- * @return [ Object ]
- */
-exports.getSettings = function()
-{
-    return this._settings || {};
-};
-
-/**
- * Overwrite the default settings.
- *
- * @param [ Object ] overrides Dict of options to be overridden.
- *
- * @return [ Void ]
- */
-exports.setDefaults = function (overrides, fn)
-{
-    var defaults = this.getDefaults();
-
-    for (var key in defaults)
-    {
-        if (overrides.hasOwnProperty(key))
-        {
-            defaults[key] = overrides[key];
-        }
-    }
-
-    if (this._isAndroid)
-    {
-        cordova.exec((fn || function () {}), null, 'BackgroundMode', 'configure', [defaults, false]);
-    }
-};
-
-/**
- * Configures the notification settings for Android.
- * Will be merged with the defaults.
- *
- * @param [ Object ] options Dict of options to be overridden.
- *
- * @return [ Void ]
- */
-exports.configure = function (options)
-{
-    var settings = this.getSettings(),
-        defaults = this.getDefaults();
-
-    if (!this._isAndroid)
-        return;
-
-    if (!this._isActive)
-    {
-        console.log('BackgroundMode is not active, skipped...');
-        return;
-    }
-
-    this._mergeObjects(options, settings);
-    this._mergeObjects(options, defaults);
-    this._settings = options;
-
-    cordova.exec(null, null, 'BackgroundMode', 'configure', [options, true]);
-};
-
-/**
- * Enable GPS-tracking in background (Android).
- *
- * @return [ Void ]
- */
-exports.disableWebViewOptimizations = function()
-{
-    if (this._isAndroid)
-    {
-        cordova.exec(null, null, 'BackgroundModeExt', 'webview', []);
-    }
-};
-
-/**
- * Disables battery optimazation mode for the app.
- *
- * @return [ Void ]
- */
-exports.disableBatteryOptimizations = function()
-{
-    if (this._isAndroid)
-    {
-        cordova.exec(null, null, 'BackgroundModeExt', 'battery', []);
-    }
-};
-
-/**
- * Opens the system settings dialog where the user can tweak or turn off any
- * custom app start settings added by the manufacturer if available.
- *
- * @param [ Object|Bool ] options Set to false if you dont want to display an
- *                                alert dialog first.
- *
- * @return [ Void ]
- */
-exports.openAppStartSettings = function (options)
-{
-    if (this._isAndroid)
-    {
-        cordova.exec(null, null, 'BackgroundModeExt', 'appstart', [options]);
-    }
-};
-
-/**
- * Move app to background (Android only).
- *
- * @return [ Void ]
- */
-exports.moveToBackground = function()
-{
-    if (this._isAndroid)
-    {
-        cordova.exec(null, null, 'BackgroundModeExt', 'background', []);
-    }
-};
-
-/**
- * Move app to foreground when in background (Android only).
- *
- * @return [ Void ]
- */
-exports.moveToForeground = function()
-{
-    if (this.isActive() && this._isAndroid)
-    {
-        cordova.exec(null, null, 'BackgroundModeExt', 'foreground', []);
-    }
-};
-
-/**
- * Exclude the app from the recent tasks list (Android only).
- *
- * @return [ Void ]
- */
-exports.excludeFromTaskList = function()
-{
-    if (this._isAndroid)
-    {
-        cordova.exec(null, null, 'BackgroundModeExt', 'tasklistExclude', []);
-    }
-};
-
-/**
- * Include the app back to the recent tasks list (Android only).
- *
- * @return [ Void ]
- */
-exports.includeToTaskList = function()
-{
-    if (this._isAndroid)
-    {
-        cordova.exec(null, null, 'BackgroundModeExt', 'tasklistInclude', []);
-    }
-};
-
-/**
- * Override the back button on Android to go to background
- * instead of closing the app.
- *
- * @return [ Void ]
- */
-exports.overrideBackButton = function()
-{
-    document.addEventListener('backbutton', function() {
-        exports.moveToBackground();
-    }, false);
-};
-
-/**
- * If the screen is off.
- *
- * @param [ Function ] fn Callback function to invoke with boolean arg.
- *
- * @return [ Void ]
- */
-exports.isScreenOff = function (fn)
-{
-    if (this._isAndroid)
-    {
-        cordova.exec(fn, null, 'BackgroundModeExt', 'dimmed', []);
-    }
-    else
-    {
-        fn(undefined);
-    }
-};
-
-/**
- * Wake up the device.
- *
- * @return [ Void ]
- */
-exports.wakeUp = function()
-{
-    if (this._isAndroid)
-    {
-        cordova.exec(null, null, 'BackgroundModeExt', 'wakeup', []);
-    }
-};
-
-/**
- * Wake up and unlock the device.
- *
- * @return [ Void ]
- */
-exports.unlock = function()
-{
-    if (this._isAndroid)
-    {
-        cordova.exec(null, null, 'BackgroundModeExt', 'unlock', []);
+    this._isAndroid = device.platform.match(/^android|amazon/i) !== null;	
+	this._isEnabled = false;
+	this._isActive = false;
+		
+	if (this._isAndroid) {		
+		this.on('activate', function() {
+			exports._isActive = true;
+			// reset runtime settings to defaults
+			exports._settings = exports._mergeObjects({}, exports._defaults);
+		});
+		this.on('deactivate', function() {
+			exports._isActive = false;
+			// reset runtime settings to unset
+			exports._settings = {};
+		});
+	}
+	else if (device.platform == 'browser') {
+        this._isEnabled = true;
+        this._isActive = true;
     }
 };
 
@@ -324,6 +76,341 @@ exports.isActive = function()
     return this._isActive !== false;
 };
 
+/**
+ * @private
+ *
+ * Default values of all available options.
+ */
+exports._defaults =
+{
+    title:   undefined,
+    text:    undefined,
+    bigText: false,
+    resume:  true,
+    color:   undefined,
+    icon:    undefined
+};
+
+/**
+ * List of all available options with their default value.
+ *
+ * @return [ Object ]
+ */
+exports.getDefaults = function()
+{
+    return this._defaults;
+};
+
+/**
+ * Overwrite the default settings.
+ *
+ * @param [ Object ] overrides Dict of options to be overridden.
+ *
+ * @return [ Void ]
+ */
+exports.setDefaults = function (overrides)
+{
+	for (var key in this._defaults) {
+		if (overrides.hasOwnProperty(key)) {
+			this._defaults[key] = overrides[key];
+		}
+	}
+	
+    if (this._isAndroid) {
+        cordova.exec(null, null, 'BackgroundMode', 'configure', [this._defaults, false]);
+    }
+};
+
+/**
+ * List of current runtime settings
+ *
+ * @return [ Object ]
+ */
+exports.getSettings = function()
+{
+	return this._settings || {};
+};
+
+/**
+ * Configures the notification settings for Android.
+ * Will be merged with the defaults.
+ *
+ * @param [ Object ] options Dict of options to be overridden.
+ *
+ * @return [ Void ]
+ */
+exports.configure = function (options)
+{
+    if (this._isAndroid) {
+		if (!this._isActive) {
+            console.log('Background Mode is not active, configuration skipped...');
+            return;
+        }
+		
+		this._mergeObjects(options, this._settings);
+        this._mergeObjects(options, this._defaults);
+        this._settings = options;
+		
+        cordova.exec(null, null, 'BackgroundMode', 'configure', [options, true]);
+    }
+};
+
+/**
+ * Activates the background mode. When activated the application
+ * will be prevented from going to sleep while in background
+ * for the next time.
+ *
+ * @param [ Function ] success Callback on success
+ * @param [ Function ] error Callback on error
+ *
+ * @return [ Void ]
+ */
+exports.enable = function(success, error)
+{
+    if (this._isEnabled) {
+        success();
+        return;
+    }
+
+    var onSuccess = function() {
+        this._isEnabled = true;
+        this.fireEvent('enable');
+        success();
+    };
+    var onError = function(errorMsg) {
+        error(errorMsg);
+    };
+
+    cordova.exec(onSuccess, onError, 'BackgroundMode', 'enable', []);
+};
+
+/**
+ * Deactivates the background mode. When deactivated the application
+ * will not stay awake while in background.
+ *
+ * @param [ Function ] success Callback on success
+ * @param [ Function ] error Callback on error
+ *
+ * @return [ Void ]
+ */
+exports.disable = function(success, error)
+{
+    if (!this._isEnabled) {
+        success();
+        return;
+    }
+
+    var onSuccess = function() {
+        this._isEnabled = false;
+        this.fireEvent('disable');
+        success();
+    };
+    var onError = function(errorMsg) {
+        error(errorMsg);
+    };
+
+    cordova.exec(onSuccess, onError, 'BackgroundMode', 'disable', []);
+};
+
+/**
+ * Enable or disable the background mode.
+ *
+ * @param [ Bool ] enable The status to set for.
+ * @param [ Function ] success Callback on success
+ * @param [ Function ] error Callback on error
+ *
+ * @return [ Void ]
+ */
+exports.setEnabled = function (enable, success, error)
+{
+    if (enable) {
+        this.enable(success, error);
+    } else {
+        this.disable(success, error);
+    }
+};
+
+/**
+ * Enable GPS-tracking in background (Android).
+ *
+ * @return [ Void ]
+ */
+exports.disableWebViewOptimizations = function()
+{
+    if (this._isAndroid) {
+        cordova.exec(null, null, 'BackgroundModeExt', 'webview', []);
+    }
+};
+
+/**
+ * Disables battery optimazation mode for the app.
+ *
+ * @return [ Void ]
+ */
+exports.disableBatteryOptimizations = function()
+{
+    if (this._isAndroid) {
+        cordova.exec(null, null, 'BackgroundModeExt', 'battery', []);
+    }
+};
+
+/**
+ * Opens the system settings dialog where the user can tweak or turn off any
+ * custom app start settings added by the manufacturer if available.
+ *
+ * @param [ Object|Bool ] options Set to false if you dont want to display an
+ *                                alert dialog first.
+ *
+ * @return [ Void ]
+ */
+exports.openAppStartSettings = function (options)
+{
+    if (this._isAndroid) {
+        cordova.exec(null, null, 'BackgroundModeExt', 'appstart', [options]);
+    }
+};
+
+/**
+ * Move app to background (Android only).
+ *
+ * @return [ Void ]
+ */
+exports.moveToBackground = function()
+{
+    if (this._isAndroid) {
+        cordova.exec(null, null, 'BackgroundModeExt', 'background', []);
+    }
+};
+
+/**
+ * Move app to foreground when in background (Android only).
+ *
+ * @return [ Void ]
+ */
+exports.moveToForeground = function()
+{
+    if (this._isAndroid) {
+        cordova.exec(null, null, 'BackgroundModeExt', 'foreground', []);
+    }
+};
+
+/**
+ * Exclude the app from the recent tasks list (Android only).
+ *
+ * @return [ Void ]
+ */
+exports.excludeFromTaskList = function()
+{
+    if (this._isAndroid) {
+        cordova.exec(null, null, 'BackgroundModeExt', 'tasklistExclude', []);
+    }
+};
+
+/**
+ * Include the app back to the recent tasks list (Android only).
+ *
+ * @return [ Void ]
+ */
+exports.includeToTaskList = function()
+{
+    if (this._isAndroid) {
+        cordova.exec(null, null, 'BackgroundModeExt', 'tasklistInclude', []);
+    }
+};
+
+/**
+ * Override the back button on Android to go to background
+ * instead of closing the app.
+ *
+ * @return [ Void ]
+ */
+exports.overrideBackButton = function()
+{
+    if (this._isAndroid) {
+		document.addEventListener('backbutton', exports.moveToBackground, false);
+    }
+};
+
+/**
+ * If the screen is off.
+ *
+ * @param [ Function ] fn Callback function to invoke with boolean arg.
+ *
+ * @return [ Void ]
+ */
+exports.isScreenOff = function (fn)
+{
+    if (this._isAndroid) {
+        cordova.exec(fn, null, 'BackgroundModeExt', 'dimmed', []);
+    }
+    else {
+        fn(undefined);
+    }
+};
+
+/**
+ * Wake up the device.
+ *
+ * @return [ Void ]
+ */
+exports.wakeUp = function()
+{
+    if (this._isAndroid) {
+        cordova.exec(null, null, 'BackgroundModeExt', 'wakeup', []);
+    }
+};
+
+/**
+ * Wake up and unlock the device.
+ *
+ * @return [ Void ]
+ */
+exports.unlock = function()
+{
+    if (this._isAndroid) {
+        cordova.exec(null, null, 'BackgroundModeExt', 'unlock', []);
+    }
+};
+
+/**
+ * Request notification permissions (Android 13+)
+ *
+ * @param [ Function ] success Callback on success
+ * @param [ Function ] error Callback on error
+ * 
+ * @return [ Void ]
+ */
+exports.requestPermissions = function(success, error)
+{
+    if (this._isAndroid) {
+        cordova.exec(success, error, 'BackgroundMode', 'requestPermissions', []);
+    } else {
+        // others dont need this
+        if (success) success();
+    }
+};
+
+/**
+ * @private
+ *
+ * Merge settings with default values.
+ *
+ * @param [ Object ] options The custom options.
+ * @param [ Object ] toMergeIn The options to merge in.
+ *
+ * @return [ Object ] Default values merged with custom values.
+ */
+exports._mergeObjects = function (options, toMergeIn)
+{
+    for (var key in toMergeIn) {
+        if (!options.hasOwnProperty(key)) {
+            options[key] = toMergeIn[key];
+        }
+    }
+
+    return options;
+};
+
 exports._listener = {};
 
 /**
@@ -342,8 +429,7 @@ exports.fireEvent = function (event)
     if (!listener)
         return;
 
-    for (var i = 0; i < listener.length; i++)
-    {
+    for (var i = 0; i < listener.length; i++) {
         var fn    = listener[i][0],
             scope = listener[i][1];
 
@@ -362,11 +448,9 @@ exports.fireEvent = function (event)
  */
 exports.on = function (event, callback, scope)
 {
-    if (typeof callback !== "function")
-        return;
+    if (typeof callback !== "function") return;
 
-    if (!this._listener[event])
-    {
+    if (!this._listener[event]) {
         this._listener[event] = [];
     }
 
@@ -383,134 +467,19 @@ exports.on = function (event, callback, scope)
  *
  * @return [ Void ]
  */
-exports.un = function (event, callback)
+exports.off = function (event, callback)
 {
     var listener = this._listener[event];
 
-    if (!listener)
-        return;
+    if (!listener) return;
 
-    for (var i = 0; i < listener.length; i++)
-    {
+    for (var i = 0; i < listener.length; i++) {
         var fn = listener[i][0];
 
-        if (fn == callback)
-        {
+        if (fn == callback) {
             listener.splice(i, 1);
             break;
         }
-    }
-};
-
-/**
- * @private
- *
- * Flag indicates if the mode is enabled.
- */
-exports._isEnabled = false;
-
-/**
- * @private
- *
- * Flag indicates if the mode is active.
- */
-exports._isActive = false;
-
-/**
- * @private
- *
- * Default values of all available options.
- */
-exports._defaults =
-{
-    title:   undefined,
-    text:    undefined,
-    bigText: false,
-    resume:  true,
-    silent:  false,
-    color:   undefined,
-    icon:    undefined
-};
-
-/**
- * @private
- *
- * Merge settings with default values.
- *
- * @param [ Object ] options The custom options.
- * @param [ Object ] toMergeIn The options to merge in.
- *
- * @return [ Object ] Default values merged with custom values.
- */
-exports._mergeObjects = function (options, toMergeIn)
-{
-    for (var key in toMergeIn)
-    {
-        if (!options.hasOwnProperty(key))
-        {
-            options[key] = toMergeIn[key];
-            continue;
-        }
-    }
-
-    return options;
-};
-
-/**
- * @private
- *
- * Setter for the isActive flag. Resets the
- * settings if the mode isnt active anymore.
- *
- * @param [ Boolean] value The new value for the flag.
- *
- * @return [ Void ]
- */
-exports._setActive = function(value)
-{
-    if (this._isActive == value)
-        return;
-
-    this._isActive = value;
-    this._settings = value ? this._mergeObjects({}, this._defaults) : {};
-};
-
-/**
- * @private
- *
- * Initialize the plugin.
- *
- * Method should be called after the 'deviceready' event
- * but before the event listeners will be called.
- *
- * @return [ Void ]
- */
-exports._pluginInitialize = function()
-{
-    this._isAndroid = device.platform.match(/^android|amazon/i) !== null;
-    if (device.platform == 'browser')
-    {
-        this.setDefaults({});
-        this.enable();
-        this._isEnabled = true;
-        this._isActive = true;
-    }
-};
-
-/**
- * Request notification permissions (Android 13+)
- *
- * @param [ Function ] success Callback on success
- * @param [ Function ] error Callback on error
- * 
- * @return [ Void ]
- */
-exports.requestPermissions = function(success, error) {
-    if (this._isAndroid) {
-        cordova.exec(success, error, 'BackgroundMode', 'requestPermissions', []);
-    } else {
-        // iOS doesn't need this
-        if (success) success();
     }
 };
 
@@ -525,13 +494,11 @@ channel.onCordovaReady.subscribe(function()
 // Called after 'deviceready' event
 channel.deviceready.subscribe(function()
 {
-    if (exports.isEnabled())
-    {
+    if (exports._isEnabled) {
         exports.fireEvent('enable');
     }
 
-    if (exports.isActive())
-    {
+    if (exports._isActive) {
         exports.fireEvent('activate');
     }
 });
