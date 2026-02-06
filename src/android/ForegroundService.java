9d4fad56a02163a531e22122ac37a1d9e4b558bd
tiperes <66407614+tiperes@users.noreply.github.com>
2026-02-06 16:24:12 +0000

    Added support to recover from notification swipes

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -19,14 +19,33 @@ import org.json.JSONObject;
 
 public class ForegroundService extends Service {
     
-    public static final String ACTION_NOTIFICATION = "ForegroundService.NOTIFICATION";
-
     public static final int NOTIFICATION_ID = 101;
+    public static final String ACTION_UPDATE = "ForegroundService.UPDATE";
+    public static final String ACTION_RECOVER = "ForegroundService.RECOVER";
+    
     private static final String CHANNEL_ID = "background_mode_channel";
     private static final String CHANNEL_NAME = "Background Mode";
     private static final String NOTIFICATION_TITLE = "App is running in background";
     private static final String NOTIFICATION_TEXT = "Doing heavy tasks.";
     private static final String NOTIFICATION_ICON = "ic_launcher";
+    
+    private JSONObject lastSettings = null;
+    private JSONObject getSettings() {
+        if (lastSettings == null) {
+            lastSettings = BackgroundMode.getDefaultSettings();
+        }
+        return lastSettings;
+    }
+    
+    private JSONObject getSettings(JSONObject newSettings) {
+        if (newSettings == null) {
+            lastSettings = BackgroundMode.getDefaultSettings();
+        }
+        else {
+            lastSettings = newSettings;
+        }
+        return lastSettings;
+    }
 
     @Override
     public IBinder onBind (Intent intent) {
@@ -36,28 +55,8 @@ public class ForegroundService extends Service {
 
     @Override
     public void onCreate() {
-        super.onCreate();
-        
-        ensureNotificationChannel();
-        Notification notification = makeNotification(BackgroundMode.getDefaultSettings());
-
-        // Android 12+
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            int fgsTypes =
-                    ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC |
-                    ServiceInfo.FOREGROUND_SERVICE_TYPE_REMOTE_MESSAGING;
-            startForeground(NOTIFICATION_ID, notification, fgsTypes);
-        // Android 10+
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            startForeground(
-                    NOTIFICATION_ID,
-                    notification,
-                    ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
-            );
-        // Older than Android 10
-        } else {
-            startForeground(NOTIFICATION_ID, notification);
-        }
+        super.onCreate();        
+        startForegroundSafe(getSettings());
     }
 
     @Override
@@ -68,27 +67,21 @@ public class ForegroundService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent != null && ACTION_NOTIFICATION.equals(intent.getAction())) {
-            try {
-                String raw = intent.getStringExtra("settings");
-                if (raw != null) {
-                    updateNotification(new JSONObject(raw));
-                }
-            } catch (Exception ignored) {}
+        if (intent != null) {
+            String action = intent.getAction();
+            
+            if (ACTION_UPDATE.equals(action)) {
+                try {
+                    JSONObject newSettings = new JSONObject(intent.getStringExtra("settings"));
+                    updateNotification(getSettings(newSettings));
+                } catch (Exception ignored) {}
+            }
+            else if (ACTION_RECOVER.equals(action)) {
+                startForegroundSafe(getSettings());
+            }
         }
         // allows recovery if system kills service, ignored if user stopped
-        // onCreate is execute and then onStartCommand will also with the last intent received.
-        return START_REDELIVER_INTENT;
-    }
-
-    /**
-     * Update existing notification
-     */
-    public void updateNotification(JSONObject settings) {
-        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        if (nm == null) return;
-        
-        nm.notify(NOTIFICATION_ID, makeNotification(settings));
+        return START_STICKY;
     }
 
     private void ensureNotificationChannel() {
@@ -109,6 +102,39 @@ public class ForegroundService extends Service {
             channel.enableVibration(false);
             nm.createNotificationChannel(channel);
         }
+    }
+
+    private void startForegroundSafe(JSONObject settings) {
+        ensureNotificationChannel();
+        
+        Notification notification = makeNotification(settings);
+        // Android 12+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            int fgsTypes =
+                    ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC |
+                    ServiceInfo.FOREGROUND_SERVICE_TYPE_REMOTE_MESSAGING;
+            startForeground(NOTIFICATION_ID, notification, fgsTypes);
+        // Android 10+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(
+                    NOTIFICATION_ID,
+                    notification,
+                    ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
+            );
+        // Older than Android 10
+        } else {
+            startForeground(NOTIFICATION_ID, notification);
+        }
+    }
+    
+    /**
+     * Update existing notification
+     */
+    private void updateNotification(JSONObject settings) {
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (nm == null) return;
+        
+        nm.notify(NOTIFICATION_ID, makeNotification(settings));
     }
 
     private Notification makeNotification(JSONObject settings) {
@@ -137,6 +163,11 @@ public class ForegroundService extends Service {
 
         setColor(builder, settings);
 
+        int intentFlags = PendingIntent.FLAG_UPDATE_CURRENT;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            intentFlags |= PendingIntent.FLAG_IMMUTABLE;
+        }
+
         if (resume) {
             Intent intent = context.getPackageManager()
                     .getLaunchIntentForPackage(context.getPackageName());
@@ -147,21 +178,28 @@ public class ForegroundService extends Service {
                         Intent.FLAG_ACTIVITY_SINGLE_TOP
                 );
 
-                int flags = PendingIntent.FLAG_UPDATE_CURRENT;
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    flags |= PendingIntent.FLAG_IMMUTABLE;
-                }
-
                 builder.setContentIntent(
                         PendingIntent.getActivity(
                                 context,
                                 NOTIFICATION_ID,
                                 intent,
-                                flags
+                                intentFlags
                         )
                 );
             }
         }
+
+        // Add deleteIntent to handle user swipes
+        Intent deleteIntent = new Intent(context, ForegroundService.class);
+        deleteIntent.setAction(ACTION_RECOVER);
+        builder.setDeleteIntent(
+            PendingIntent.getService(
+                context,
+                NOTIFICATION_ID,
+                deleteIntent,
+                intentFlags
+            )
+        );
 
         return builder.build();
     }
